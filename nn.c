847550b5e3ba9fb513deafdc6a39fcff6dc07bca
William Hart <hart_wb@yahoo.com>
2010-09-12 23:52:20 +0100

    More function renames.

--- nn.c
+++ nn.c
@@ -76,7 +76,7 @@ word_t nn_add_mc(nn_t a, nn_src_t b, nn_src_t c, len_t m, word_t ci)
    return ci;
 }
 
-word_t _nn_sub_mc(nn_t a, nn_src_t b, nn_src_t c, len_t m, word_t bi)
+word_t nn_sub_mc(nn_t a, nn_src_t b, nn_src_t c, len_t m, word_t bi)
 {
    dword_t t;
    long i;
@@ -91,7 +91,7 @@ word_t _nn_sub_mc(nn_t a, nn_src_t b, nn_src_t c, len_t m, word_t bi)
    return bi;
 }
 
-word_t _nn_shl_c(nn_t a, nn_src_t b, len_t m, bits_t bits, word_t ci)
+word_t nn_shl_c(nn_t a, nn_src_t b, len_t m, bits_t bits, word_t ci)
 {
    dword_t t;
    long i;
@@ -106,7 +106,7 @@ word_t _nn_shl_c(nn_t a, nn_src_t b, len_t m, bits_t bits, word_t ci)
    return ci;
 }
 
-word_t _nn_shr_c(nn_t a, nn_src_t b, len_t m, bits_t bits, word_t ci)
+word_t nn_shr_c(nn_t a, nn_src_t b, len_t m, bits_t bits, word_t ci)
 {
    dword_t t;
    long i;
