ef57c1b80543195a08f0596e9dae6f334eaa2644
William Hart <hart_wb@yahoo.com>
2010-09-12 01:03:15 +0100

    Added some chain2, chain3 test macros.

--- t-nn.c
+++ t-nn.c
@@ -24,12 +24,35 @@ rand_t state;
       __result = 0; \
    }
 
-int test_add_m_new(void)
+#define chain2_rl_1d_2s_test(fn, r, s, a, b, m, n) \
+   do { \
+      m = randint(100, state); \
+      n = randint(100, state); \
+      new_objs(NN, &a, &b, &r, &s, NULL); \
+      randoms_of_len(m + n + 1, ANY, state, r, s, NULL); \
+      randoms_of_len(m + n, ANY, state, a, b, NULL); \
+      chain2_rl_1d_2s(fn, r, a, b, m, n); \
+      fn(s, a, b, NULL); \
+   } while (0)
+ 
+#define chain3_rl_1d_2s_test(fn, r, s, a, b, m, n, p) \
+   do { \
+      m = randint(100, state); \
+      n = randint(100, state); \
+      p = randint(100, state); \
+      new_objs(NN, &a, &b, &r, &s, NULL); \
+      randoms_of_len(m + n + p + 1, ANY, state, r, s, NULL); \
+      randoms_of_len(m + n + p, ANY, state, a, b, NULL); \
+      chain3_rl_1d_2s(fn, r, a, b, m, n, p); \
+      fn(s, a, b, NULL); \
+   } while (0)
+
+int test_add_m(void)
 {
    len_t m, n, p;
    obj_t * a, * b, * c, * r, * s;
    
-   printf("add_m_new...");
+   printf("add_m...");
 
    TEST_START(ITER) /* test (a + b) + c != (a + c) + b */
    {
@@ -50,123 +73,20 @@ int test_add_m_new(void)
    } TEST_END;
 
    TEST_START(ITER) /* test chaining of two add_m's */
-   {
-      m = randint(100, state);
-      n = randint(100, state);
-      
-      new_objs(NN, &a, &b, &r, &s, NULL);
-      
-      randoms_of_len(m + n + 1, ANY, state, r, s, NULL);
-      randoms_of_len(m + n, ANY, state, a, b, NULL);
-      
-      chain2_rl_1d_2s(add_m, r, a, b, m, n);
-      
-      add_m(s, a, b, NULL);
+   {   
+      chain2_rl_1d_2s_test(add_m, r, s, a, b, m, n);
       
       ASSERT_EQUAL(r, s, "add_m fails chain2");
    } TEST_END;
 
    TEST_START(ITER) /* test chaining of three add_m's */
-   {
-      m = randint(100, state);
-      n = randint(100, state);
-      p = randint(100, state);
-      
-      new_objs(NN, &a, &b, &r, &s, NULL);
-      
-      randoms_of_len(m + n + p + 1, ANY, state, r, s, NULL);
-      randoms_of_len(m + n + p, ANY, state, a, b, NULL);
-      
-      chain3_rl_1d_2s(add_m, r, a, b, m, n, p);
-      
-      add_m(s, a, b, NULL);
-      
+   {      
+      chain3_rl_1d_2s_test(add_m, r, s, a, b, m, n, p);
+
       ASSERT_EQUAL(r, s, "add_m fails chain3");
    } TEST_END;
 
    return 1;
-}
-
-int test_add_m(void)
-{
-   int result = 1;
-   long i;
-   nn_t a, b, c, r1, r2;
-   len_t m, n;
-   word_t ci;
-
-   printf("nn_add_m...");
-
-   /* test (a + b) + c = a + (b + c) */
-   for (i = 0; i < ITER && result == 1; i++)
-   {
-      m = randint(100, state);
-
-      a = nn_init(m);
-      b = nn_init(m);
-      c = nn_init(m);
-
-      r1 = nn_init(m + 1);
-      r2 = nn_init(m + 1);
-
-      nn_random(a, state, m);
-      nn_random(b, state, m);
-      nn_random(c, state, m);
-
-      nn_add_m(r1, a, b, m);
-      nn_add_m(r1, r1, c, m);
-
-      nn_add_m(r2, b, c, m);
-      nn_add_m(r2, r2, a, m);
-
-      result = nn_equal_m(r1, r2, m + 1);
-
-      if (!result)
-      {
-         printf("m = %ld\n", m);
-      }
-
-      nn_clear(a);
-      nn_clear(b);
-      nn_clear(c);
-      nn_clear(r1);
-      nn_clear(r2);
-   }
-
-   /* test chaining of addition */
-   for (i = 0; i < ITER && result == 1; i++)
-   {
-      m = randint(100, state);
-      n = randint(100, state);
-
-      a = nn_init(m + n);
-      b = nn_init(m + n);
-
-      r1 = nn_init(m + n + 1);
-      r2 = nn_init(m + n + 1);
-
-      nn_random(a, state, m + n);
-      nn_random(b, state, m + n);
-      
-      ci = _nn_add_m(r1, a, b, m);
-      nn_add_mc(r1 + m, a + m, b + m, n, ci);
-
-      nn_add_m(r2, a, b, m + n);
-      
-      result = nn_equal_m(r1, r2, m + n + 1);
-
-      if (!result)
-      {
-         printf("m = %ld, n = %ld\n", m, n);
-      }
-
-      nn_clear(a);
-      nn_clear(b);
-      nn_clear(r1);
-      nn_clear(r2);
-   }
-
-   return result;
 }
 
 int test_add(void)
@@ -2012,7 +1932,6 @@ int main(void)
    
    randinit(state);
 
-   RUN(test_add_m_new);
    RUN(test_not);
    RUN(test_neg);
    RUN(test_add1);
