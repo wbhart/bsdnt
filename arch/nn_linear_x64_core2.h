ba9ec98e27e1e4c1b2b3f7596d12456ffc870cb3
William Hart <hart_wb@yahoo.com>
2010-09-19 22:02:29 +0100

    Added core2 assembly for nn_add/sub_mc.

---
+++ arch/nn_linear_x64_core2.h
@@ -0,0 +1,66 @@
+/* Copyright 2010 (C) Gonzalo Tornaria, Antony Vennard, Brian Gladman, William Hart */
+
+#define HAVE_ARCH_nn_add_mc
+
+word_t nn_add_mc(nn_t a, nn_src_t b, nn_src_t c, len_t m, word_t ci)
+{
+   __asm__ (
+
+   ".intel_syntax noprefix; \
+    mov rax, r8; \
+    jrcxz 3f; \
+    lea rdi, [rdi+rcx*8]; \
+    lea rsi, [rsi+rcx*8]; \
+    lea rdx, [rdx+rcx*8]; \
+    neg rcx; \
+    bt  rax, 0; \
+1:; \
+    mov r10, [rsi+rcx*8]; \
+    adc r10, [rdx+rcx*8]; \
+    mov [rdi+rcx*8], r10; \
+    lea rcx, [rcx+1]; \
+    jrcxz 2f; \
+    jmp 1b; \
+2:; \
+    setc al; \
+3:;"
+
+   : "=a" ((word_t)(ci))
+   : "c" ((len_t)(m)), "d" ((nn_src_t)(c)), "S" ((nn_src_t)(b)), "D" ((nn_src_t *)(a))
+   );
+
+   return ci;
+}
+
+#define HAVE_ARCH_nn_sub_mc
+
+word_t nn_sub_mc(nn_t a, nn_src_t b, nn_src_t c, len_t m, word_t ci)
+{
+   __asm__ (
+
+   ".intel_syntax noprefix; \
+    mov rax, r8; \
+    jrcxz 3f; \
+    lea rdi, [rdi+rcx*8]; \
+    lea rsi, [rsi+rcx*8]; \
+    lea rdx, [rdx+rcx*8]; \
+    neg rcx; \
+    bt  rax, 0; \
+1:; \
+    mov r10, [rsi+rcx*8]; \
+    sbb r10, [rdx+rcx*8]; \
+    mov [rdi+rcx*8], r10; \
+    lea rcx, [rcx+1]; \
+    jrcxz 2f; \
+    jmp 1b; \
+2:; \
+    setc al; \
+3:;"
+
+   : "=a" ((word_t)(ci))
+   : "c" ((len_t)(m)), "d" ((nn_src_t)(c)), "S" ((nn_src_t)(b)), "D" ((nn_src_t *)(a))
+   );
+
+   return ci;
+}
+
