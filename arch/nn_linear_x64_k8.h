51df4bd5220d2e957e69def145380177e4b10944
William Hart <hart_wb@yahoo.com>
2010-09-19 21:29:03 +0100

    Added nn_sub_mc assembly.

--- arch/nn_linear_x64_k8.h
+++ arch/nn_linear_x64_k8.h
@@ -30,3 +30,33 @@ word_t nn_add_mc(nn_t a, nn_src_t b, nn_src_t c, len_t m, word_t ci)
    return ci;
 }
 
+#define HAVE_ARCH_nn_sub_mc
+
+word_t nn_sub_mc(nn_t a, nn_src_t b, nn_src_t c, len_t m, word_t ci)
+{
+   __asm__ (
+
+   ".intel_syntax noprefix; \
+    mov rax, r8; \
+    jrcxz 2f; \
+    lea rdi, [rdi+rcx*8]; \
+    lea rsi, [rsi+rcx*8]; \
+    lea rdx, [rdx+rcx*8]; \
+    neg rcx; \
+    sar eax, 1; \
+1:; \
+    mov r10, [rsi+rcx*8]; \
+    sbb r10, [rdx+rcx*8]; \
+    mov [rdi+rcx*8], r10; \
+    inc rcx; \
+    jnz 1b; \
+    setc al; \
+2:; "
+
+   : "=a" ((word_t)(ci))
+   : "c" ((len_t)(m)), "d" ((nn_src_t)(c)), "S" ((nn_src_t)(b)), "D" ((nn_src_t *)(a))
+   );
+
+   return ci;
+}
+
