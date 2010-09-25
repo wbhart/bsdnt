25036b2bb3a3d675da20f441c88a7a13adbc4ce8
William Hart <hart_wb@yahoo.com>
2010-09-25 18:31:51 +0100

    Minor spacing issue.

--- nn.h
+++ nn.h
@@ -865,7 +865,7 @@ word_t nn_muladd_classical(nn_t r, nn_src_t a, nn_src_t b,
    must have space for m - n + 1 limbs. 
 */
 void nn_divrem_classical_preinv_c(nn_t q, nn_t a, len_t m, nn_src_t d, 
-                                     len_t n,preinv1_2_t inv, word_t ci);
+                                     len_t n, preinv1_2_t inv, word_t ci);
 
 /*
    As per nn_divrem_classical_preinv_c except that the carry-in is read 
