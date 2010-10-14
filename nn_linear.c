17b9989aec9c7ca947fd58466877ba063439c8b0
William Hart <hart_wb@yahoo.com>
2010-10-15 00:14:08 +0100

    Added some missing guards.

--- nn_linear.c
+++ nn_linear.c
@@ -139,6 +139,8 @@ word_t nn_shr_c(nn_t a, nn_src_t b, len_t m, bits_t bits, word_t ci)
 
 #endif
 
+#ifndef HAVE_ARCH_nn_add1
+
 word_t nn_add1(nn_t a, nn_src_t b, len_t m, word_t c)
 {
    dword_t t;
@@ -158,6 +160,10 @@ word_t nn_add1(nn_t a, nn_src_t b, len_t m, word_t c)
    return c;
 }
 
+#endif
+
+#ifndef HAVE_ARCH_nn_sub1
+
 word_t nn_sub1(nn_t a, nn_src_t b, len_t m, word_t c)
 {
    dword_t t;
@@ -176,6 +182,8 @@ word_t nn_sub1(nn_t a, nn_src_t b, len_t m, word_t c)
 
    return c;
 }
+
+#endif
 
 #ifndef HAVE_ARCH_nn_neg_c
 
