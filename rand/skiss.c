5be1d5837a01fcc2b86b5c862ff0b8916c99a1c1
William Hart <hart_wb@yahoo.com>
2010-11-11 13:47:46 +0000

    Combine 32 and 64 bit PRNGs into a single file for each PRNG
    Conflicts:
    
    	bsdnt.vcxproj.filters

--- rand/skiss.c
+++ rand/skiss.c
@@ -1,4 +1,5 @@
 /* 
+  George Marsaglia's SUPRKISS32, period 5 * 2^1320481 * (2^32 - 1)
   George Marsaglia's SUPRKISS64, period 5 * 2^1320480 * (2^64 - 1)
 
   Copyright (C) 2010, Brian Gladman
@@ -28,11 +29,77 @@
 */
 
 #include "../helper.h"
-
-#if WORD_BITS == 64
-
 #include <stdlib.h>
 #include "internal_rand.h"
+
+#if WORD_BITS == 32
+
+typedef void * rand_ctx;
+
+typedef struct
+{   
+   word_t q[41265];
+	word_t carry;
+	word_t xcng;
+	word_t xs;
+	word_t indx;
+} skiss_ctx;
+
+#define CTX(c) ((skiss_ctx *)(c))
+
+#define CNG(p)  (p->xcng = WORD(69069) * p->xcng + WORD(123))
+#define XS(p)   (p->xs ^= p->xs << 13, p->xs ^= p->xs >> 17, \
+                  p->xs ^= p->xs << 5)
+#define SUPR(p) ((p->indx < 41265L) ? p->q[p->indx++] : refill(p))
+#define KISS(p) (SUPR(p) + CNG(p) + XS(p))
+
+rand_ctx skiss_init(void)
+{   
+   long i;
+	rand_ctx c = malloc(sizeof(skiss_ctx));
+
+   CTX(c)->carry = WORD(362);
+	CTX(c)->xcng = WORD(1236789);
+	CTX(c)->xs = WORD(521288629);
+	CTX(c)->indx = WORD(41265);
+
+   for (i = 0; i < 41265; ++i)
+      CTX(c)->q[i] = (CNG(CTX(c)) + XS(CTX(c)));
+
+   return c;
+}
+
+void skiss_clear(rand_ctx c)
+{
+	free(c);
+}
+
+word_t refill(rand_ctx c)
+{   
+   long i;
+   word_t z, h;
+
+   for (i = 0; i < 41265; ++i)
+   {
+      h = CTX(c)->carry & WORD(1);
+      z = ((CTX(c)->q[i] << 9) >> 1) + ((CTX(c)->q[i] << 7) >> 1) 
+         + (CTX(c)->carry >> 1);
+      CTX(c)->carry = (CTX(c)->q[i] >> 23) + (CTX(c)->q[i] >> 25) 
+         + (z >> 31);
+      CTX(c)->q[i] = ~((z << 1) + h);
+   }
+
+   CTX(c)->indx = WORD(1);
+    
+   return CTX(c)->q[0];
+}
+
+word_t skiss_word(rand_ctx c)
+{
+   return  KISS(CTX(c));
+}
+
+#elif WORD_BITS == 64
 
 typedef struct
 {	
