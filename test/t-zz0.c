db5157472f1f91014a009fb57f948cd590fab90b
William Hart <goodwillhart@googlemail.com>
2013-12-28 07:22:34 +0000

    Added zz0_mul test.

--- test/t-zz0.c
+++ test/t-zz0.c
@@ -155,6 +155,45 @@ int test_sub(void)
    return result;
 }
 
+int test_mul(void)
+{
+   int result = 1;
+   zz_t a, b, c, r1, r2, t1, t2;
+   len_t m1, m2, m3;
+   
+   printf("zz0_mul...");
+
+   /* test a(b + c) = ac + bc */
+   TEST_START(1, ITER) 
+   {
+      randoms_upto(10, ANY, state, &m1, &m2, &m3, NULL);
+
+      randoms_signed(m1, ANY, state, &a, NULL);
+      randoms_signed(m2, ANY, state, &b, NULL);
+      randoms_signed(m3, ANY, state, &c, NULL);
+      randoms_signed(21, ANY, state, &r1, &r2, &t1, &t2, NULL);
+      
+      t1->size = zz0_add(t1->n, b->n, b->size, c->n, c->size);
+      r1->size = zz0_mul(r1->n, a->n, a->size, t1->n, t1->size);
+
+      t1->size = zz0_mul(t1->n, a->n, a->size, b->n, b->size);
+      t2->size = zz0_mul(t2->n, a->n, a->size, c->n, c->size);
+      r2->size = zz0_add(r2->n, t1->n, t1->size, t2->n, t2->size);
+
+      result = zz_equal(r1, r2);
+
+      if (!result) 
+      {
+         bsdnt_printf("%m, %m\n", r1->size, r2->size);
+         print_debug(a->n, a->size); print_debug(b->n, b->size); 
+         print_debug(c->n, c->size); 
+         print_debug_diff(r1->n, r2->n, r1->size);
+      }
+   } TEST_END;
+
+   return result;
+}
+
 int test_zz0(void)
 {
    long pass = 0;
@@ -163,6 +202,7 @@ int test_zz0(void)
    RUN(test_gc);
    RUN(test_add);
    RUN(test_sub);
+   RUN(test_mul);
    
    printf("%ld of %ld tests pass.\n", pass, pass + fail);
 
